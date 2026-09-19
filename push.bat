c0cd4b4d7840befeb8c1cacbc3976e0f999c0ce0
hammad9885 <mrhammadghg@gmail.com>
2026-09-19 16:41:19 +0500

    Rebrand store to DigitX Pro (@digitxpro) and upgrade all product images

--- push.bat
+++ push.bat
@@ -1,50 +1,34 @@
 @echo off
 echo ===================================================
-echo   DigitX Pro - Automatic Git Commit and Push Script
+echo   DigitX Pro - Pushing to humayunchodhary/digtx
 echo ===================================================
 echo.
 
 cd /d "%~dp0"
 
-echo [1/4] Initializing Git...
-git init
+echo [1/3] Adding changes and staging...
+git add .
+git commit -m "Rebrand store to DigitX Pro (@digitxpro) and upgrade all product images" >nul 2>&1
 git branch -M main
 
-echo [2/4] Adding modified files...
-git add .
-
-echo [3/4] Committing changes...
-git commit -m "Rebrand store to DigitX Pro and upgrade all product images"
-
-echo [4/4] Setting remote and pushing to GitHub...
+echo [2/3] Setting remote repository for humayunchodhary...
 git remote remove origin >nul 2>&1
+git remote add origin https://humayunchodhary@github.com/humayunchodhary/digtx.git
 
-:: Try digtx repository
-git remote add origin https://github.com/humayunchodhary/digtx.git
+echo [3/3] Pushing to GitHub...
+echo.
 git push -u origin main
 
 if %ERRORLEVEL% NEQ 0 (
     echo.
     echo ---------------------------------------------------
-    echo Pushing to 'digtx' failed. Trying 'digitxpro'...
-    echo ---------------------------------------------------
-    git remote remove origin >nul 2>&1
-    git remote add origin https://github.com/humayunchodhary/digitxpro.git
-    git push -u origin main
-)
-
-if %ERRORLEVEL% NEQ 0 (
-    echo.
-    echo ---------------------------------------------------
-    echo Trying 'digitx'...
-    echo ---------------------------------------------------
-    git remote remove origin >nul 2>&1
-    git remote add origin https://github.com/humayunchodhary/digitx.git
+    echo Retrying with standard URL...
+    git remote set-url origin https://github.com/humayunchodhary/digtx.git
     git push -u origin main
 )
 
 echo.
 echo ===================================================
-echo   Done!
+echo   Finished!
 echo ===================================================
 pause
