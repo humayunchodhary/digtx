13207d6218c0390770377ff04fe711b9a7032805
hammad9885 <humayunchodhary@gmail.com>
2026-09-19 16:44:45 +0500

    Update push.bat

--- push.bat
+++ push.bat
@@ -11,24 +11,16 @@ git add .
 git commit -m "Rebrand store to DigitX Pro (@digitxpro) and upgrade all product images" >nul 2>&1
 git branch -M main
 
-echo [2/3] Setting remote repository for humayunchodhary...
+echo [2/3] Setting remote repository...
 git remote remove origin >nul 2>&1
 git remote add origin https://humayunchodhary@github.com/humayunchodhary/digtx.git
 
-echo [3/3] Pushing to GitHub...
+echo [3/3] Force pushing updated codebase to GitHub...
 echo.
-git push -u origin main
-
-if %ERRORLEVEL% NEQ 0 (
-    echo.
-    echo ---------------------------------------------------
-    echo Retrying with standard URL...
-    git remote set-url origin https://github.com/humayunchodhary/digtx.git
-    git push -u origin main
-)
+git push -u origin main --force
 
 echo.
 echo ===================================================
-echo   Finished!
+echo   Finished! Check https://github.com/humayunchodhary/digtx
 echo ===================================================
 pause
